a681922a2a3fe839e8f7cef7a13862398396a218
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-18 15:51:16 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import NavbarTop from './components/NavbarTop'
+import Footer from './components/Footer'
 
 import Home from './pages/home'
 import Register from './pages/register'
@@ -13,11 +14,13 @@ export default class App extends Component {
       <>
         <BrowserRouter>
           <NavbarTop/>
+          
           <Switch>
             <Route exact path='/home' component={Home}/>
             <Route exact path='/register' component={Register}/>
             <Route component={Notfound} />
           </Switch>
+          <Footer/>
         </BrowserRouter>
       </>
     )
