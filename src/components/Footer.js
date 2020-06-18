37e854212531a562984ac838002a9c230ec6deff
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-18 15:51:20 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import { Row } from 'react-bootstrap'
 
 export default class Footer extends Component {
     render() {
         return (
             <>
-                <h1>Footer</h1>
+                <Row md={12} className="no-gutters d-flex justify-content-center">
+                    <h1>Footer</h1>
+                </Row>
             </>
         )
     }
